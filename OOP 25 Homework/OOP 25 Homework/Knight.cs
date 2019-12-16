54729ab84fd2dabdee3252da7db574b06f43f915
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2019-12-16 12:58:17 +0200

    Add files via upload

---
+++ OOP 25 Homework/OOP 25 Homework/Knight.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_25_Homework
+{
+    public class Knight
+    {
+        public string Name { get; private set; }
+        public string BirthTown { get; private set; }
+        public string Title { get; private set; }
+
+        public Knight(string name, string birthTown, string title)
+        {
+            Name = name;
+            BirthTown = birthTown;
+            Title = title;
+        }
+
+        public string this[string fieldName]
+        {
+            get
+            {
+                if (fieldName == "Name")
+                    return Name;
+                if (fieldName == "BirthTown")
+                    return BirthTown;
+                if (fieldName == "Title")
+                    return Title;
+                return "UNKNOWN FIELD NAME";
+            }
+
+            set
+            {
+                if (fieldName == "Name")
+                    Name = value; ;
+                if (fieldName == "BirthTown")
+                    BirthTown = value;
+                if (fieldName == "Title")
+                    Title = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Knight --- Name: {Name}, Birth Town {BirthTown}, Title: {Title}";
+        }
+    }
+}
